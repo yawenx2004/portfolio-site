5a8c9082e43f36008956d828fe320248ad050be2
yawen xue <124210098+yawenx2004@users.noreply.github.com>
2024-04-17 03:09:32 -0400

    header & footer; password-protected internal page

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.static('fonts'));
 app.use(express.static('documents'));
 app.use(express.static('images'))
 
+// various pages
 const getHeaderHtml = () => {
   return fs.readFileSync(path.join(__dirname, 'header.html'), 'utf8');
 };
@@ -34,6 +35,39 @@ app.get('/resume', (req, res) => {
 app.get('/about', (req, res) => {
   const aboutHtml = fs.readFileSync(path.join(__dirname, 'about.html'), 'utf8');
   res.send(headerHtml + aboutHtml + footerHtml);
+});
+
+app.get('/art-gallery', (req, res) => {
+  const artGalleryHtml = fs.readFileSync(path.join(__dirname, 'art-gallery.html'), 'utf8');
+  res.send(headerHtml + artGalleryHtml + footerHtml);
+});
+
+// password-protected internal page
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+const myPassword = '1334'
+
+// password authentification form
+app.get('/internal', (req, res) => {
+  const passwordFormHtml = fs.readFileSync(path.join(__dirname, 'password-form.html'), 'utf8')
+  res.send(passwordFormHtml);
+});
+
+// handling form submission; internal page
+app.post('/authenticate', (req, res) => {
+  const { password } = req.body;
+
+  // if password is correct, send form; if not, don't
+  if (password === myPassword) {
+    const internalHtml = fs.readFileSync(path.join(__dirname, 'internal.html'), 'utf8');
+    res.send(headerHtml + internalHtml + footerHtml);
+  } else {
+    const passwordFormHtml = fs.readFileSync(path.join(__dirname, 'password-form.html'), 'utf8')
+    res.send(passwordFormHtml);
+  }
 });
 
 app.listen(port, () => {
